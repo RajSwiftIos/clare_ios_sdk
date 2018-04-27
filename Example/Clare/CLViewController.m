20a76c04675727868f5a925ee880da6f4bc9d5f8
LiuZekai <>
2018-04-27 20:29:22 +0800

    0.0.2i

--- Example/Clare/CLViewController.m
+++ Example/Clare/CLViewController.m
@@ -7,23 +7,28 @@
 //
 
 #import "CLViewController.h"
+#import <Clare/Clare.h>
 
 @interface CLViewController ()
-
-@end
+    
+    @end
 
 @implementation CLViewController
-
+    
 - (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    {
+        [super viewDidLoad];
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    [[Clare sharedManager]show];
 }
-
 - (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+    {
+        [super didReceiveMemoryWarning];
+        // Dispose of any resources that can be recreated.
+    }
+
 
 @end
