b9d5a785fd51a9fa7f735fce30ee4dde48891a85
LiuZekai <>
2018-05-04 17:40:26 +0800

    Initial commit

---
+++ Example/Clare/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Clare
+//
+//  Created by zekail on 05/04/2018.
+//  Copyright (c) 2018 zekail. All rights reserved.
+//
+
+@import UIKit;
+#import "CLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CLAppDelegate class]));
+    }
+}
