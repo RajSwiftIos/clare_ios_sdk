9651a0251861fd2fd80f543c1c3313f6d94609d2
LiuZekai <>
2018-05-15 12:49:05 +0800

    Initial commit

---
+++ Example/Clare/CLViewController.m
@@ -0,0 +1,29 @@
+//
+//  CLViewController.m
+//  Clare
+//
+//  Created by zekail on 05/15/2018.
+//  Copyright (c) 2018 zekail. All rights reserved.
+//
+
+#import "CLViewController.h"
+
+@interface CLViewController ()
+
+@end
+
+@implementation CLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
