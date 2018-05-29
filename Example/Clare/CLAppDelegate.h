5bb80690bb84d0af85a714f3a8511b0a41d6ac9a
LiuZekai <>
2018-05-29 17:51:43 +0800

    Initial commit

---
+++ Example/Clare/CLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CLAppDelegate.h
+//  Clare
+//
+//  Created by zekail on 05/29/2018.
+//  Copyright (c) 2018 zekail. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
